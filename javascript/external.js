9b97e568e0c2cb9364c09a0567737e7bbab58879
Marissa Lucke <mjlncrew3115@gmail.com>
2025-09-25 15:45:03 -0500

    Completed Task 4 Operators

--- javascript/external.js
+++ javascript/external.js
@@ -9,5 +9,13 @@
 // const out = x + y;   // combines into "webmapping"
 // document.write(out);
 // Task 3 (part 2): prompt and display user input before entering page//
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// document.write(user_name);
+// Task 4: Operators
+a = 22;            // 'a' is a variable (not declared with let/const)
+const b = 33;      // 'b' is a constant
+document.writeln(a + b);    // prints 55
+document.writeln("<br>");
+document.writeln(a += b);   // a becomes 55, prints 55
+document.writeln("<br>");
+document.writeln(a += b);   // a becomes 88, prints 88

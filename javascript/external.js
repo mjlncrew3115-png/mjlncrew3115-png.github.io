4f791b6b438be41145c0dd9ad272866d0c961416
Marissa Lucke <mjlncrew3115@gmail.com>
2025-09-25 15:39:11 -0500

    Completed Task 3 Variables

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,13 @@
 // document.write("Where is my JS code?");
 // window.alert(5 + 6); // shows 11 in an alert box
 // window.alert('5 + 6'); // shows the text literally
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>');
+// document.write('<h1>A heading</h1>');
+// document.write('<p>A sentence.</p>');
+// Task 3 (part 1): variables and concatenation
+// const x = "web";
+// const y = "mapping";
+// const out = x + y;   // combines into "webmapping"
+// document.write(out);
+// Task 3 (part 2): prompt and display user input before entering page//
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);

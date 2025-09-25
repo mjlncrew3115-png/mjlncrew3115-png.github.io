5e099b80f7adec5e5b86de25d6e3cfe768eeef28
Marissa Lucke <mjlncrew3115@gmail.com>
2025-09-25 15:50:24 -0500

    Completed Task 5 Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -1,21 +1,14 @@
-// document.write("Where is my JS code?");
-// window.alert(5 + 6); // shows 11 in an alert box
-// window.alert('5 + 6'); // shows the text literally
-// document.write('<h1>A heading</h1>');
-// document.write('<p>A sentence.</p>');
-// Task 3 (part 1): variables and concatenation
-// const x = "web";
-// const y = "mapping";
-// const out = x + y;   // combines into "webmapping"
-// document.write(out);
-// Task 3 (part 2): prompt and display user input before entering page//
-// user_name = window.prompt("Please enter your name", "Type your name here");
-// document.write(user_name);
-// Task 4: Operators
-a = 22;            // 'a' is a variable (not declared with let/const)
-const b = 33;      // 'b' is a constant
-document.writeln(a + b);    // prints 55
-document.writeln("<br>");
-document.writeln(a += b);   // a becomes 55, prints 55
-document.writeln("<br>");
-document.writeln(a += b);   // a becomes 88, prints 88
+// Task 5: Conditional statement
+document.writeln("<button onclick='condition()'>Conditional Test</button>");
+function condition()
+{
+  const x = confirm("Are you sure you want to proceed?");
+  if (x)
+  {
+    document.writeln("You chose Okay!");
+  }
+  else
+  {
+    document.writeln("You chose Cancel!");
+  }
+}

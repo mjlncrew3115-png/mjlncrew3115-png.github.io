3d771e8601dec27f63486e0d77880dc7dfe4f270
Marissa Lucke <mjlncrew3115@gmail.com>
2025-09-25 16:22:24 -0500

    Completed Lab 3

--- javascript/external.js
+++ javascript/external.js
@@ -1,6 +1,8 @@
-// Task 6: Array + for loop
-arr = ["Mael", "Eric", "Randall", "James"];
-for (let i = 0; i < arr.length; i++)
-{
-  document.writeln(arr[i], "<br>");
-}
+// Homework Part 2 (a): Prompt the user for their name
+user_name = window.prompt("Please enter your name", "Type your name here");
+
+// Homework Part 2 (b): Replace the header text with the user’s name
+// Grab the header element from index.html
+const header = document.querySelector("#header h1");
+// Replace its text content
+header.textContent = "Hello, welcome " + user_name + "!";
